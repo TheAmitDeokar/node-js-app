dfd9ceb866f230e9b6f984e3c55eee1a2fe74f97
TheAmitDeokar <49775536+TheAmitDeokar@users.noreply.github.com>
2025-01-02 18:58:06 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.111.23.2:9000/',
+  serverUrl: 'http://13.234.18.30:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_8b5bf1e9057b32ea49a542206eadec00089c43cf',
+	    'sonar.login': 'squ_f889203ba6133dee22bd97ea1c79199d258822c1',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
