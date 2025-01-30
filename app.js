929c89db06923ed8dcda45fd0b7453985a2c5278
amit deokar <amitdeokar20@gmail.com>
2025-01-31 00:05:26 +0530

    u

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('/mithuntechnologies', function(request, response) {
 app.get("/html", function(req,res){
     res.set("Content-Type","text/html");
     //res.contentType("html") ; 
-    res.write("<h2>Welcome bhau - 444</h2>");
+    res.write("<h2>Welcome bhau - 555</h2>");
     res.write("<h2>/html</h2>");
     //must end 
     res.end();
